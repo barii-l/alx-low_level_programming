a88d82156e98e20199187e077974c1e37e2bc646
bariyatlikita <bariyatlikita@gmail.com>
2022-10-13 11:31:30 -0700

    correction to task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,17 +2,11 @@
  * File: 5-printf.c
  *
  * Auth: Brennan D Baraban
- *
  */
-
-
 
 #include <stdio.h>
 
-
-
-/*
- *
+/**
  * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
  *
  * Return: Always 0.
@@ -20,11 +14,8 @@
  */
 
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
 }
