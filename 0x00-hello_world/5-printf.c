72ca3638c6410df46490524016ffbb6064f73ded
bariyatlikita <bariyatlikita@gmail.com>
2022-10-13 11:56:27 -0700

    correction to documentations

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,11 @@
-/*
- * File: 5-printf.c
- */
-
 #include <stdio.h>
 
 /*
- * main - Prints with "proper grammar, but the outcome is
- * 	 a piece of art," followed by a new line.
- *
- * Return: Always 0.
- *
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	return (0);
 }
