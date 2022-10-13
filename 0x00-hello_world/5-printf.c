b245bb138ed76512d1a3288d7d85fb27fa827cbd
bariyatlikita <bariyatlikita@gmail.com>
2022-10-13 11:25:09 -0700

    corrections to task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
+/*
+ * File: 5-printf.c
+ *
+ * Auth: Brennan D Baraban
+ *
+ */
+
+
+
+#include <stdio.h>
 
 
 
 /*
  *
- * main - Entry point
+ * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
  *
- * Description: A C program that prints with printf function.
- *
- * Return: Always zero (Success)
+ * Return: Always 0.
  *
  */
-
-
 
 int main(void)
 
