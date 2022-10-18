b48ba40743c135041603a19592c94dc66520d174
Bariyat Likita <bariyatlikita@gmail.com>
2022-10-19 00:32:17 +0100

    corrections to coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
  * main - check the code
  * print_alphabet - utilizes on the _putchar function to print the alphabet a - z
  * Return: Always 0. (Success)
 */
-
 void print_alphabet(void)
 {
 	int ch;
-
 	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
