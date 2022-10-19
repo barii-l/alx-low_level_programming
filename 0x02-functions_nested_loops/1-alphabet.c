9aaee7ff8f798e378054ea9c8f908cc0e750b834
Bariyat Likita <bariyatlikita@gmail.com>
2022-10-19 14:04:57 +0100

    corrections to task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- * print_alphabet - utilizes on the _putchar function to print the alphabet a - z
+ * print_alphabet - utilizes on the _putchar
+ * I[Ifunction to print the alphabet a - z
  * Return: Always 0. (Success)
 */
 
