e80246fa4bbabbe2302cb9d79a06aa8b78c009c5
bariyatlikita <bariyatlikita@gmail.com>
2022-10-20 18:16:02 -0700

    corrections

--- 0x03-debugging/main/2-main.c
+++ 0x03-debugging/main/2-main.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
- * main - prints the largest of 3 integers
- * Return: 0
- */
+ *
+ *  * main - prints the largest of 3 integers
+ *
+ *   * Return: 0
+ *
+ *   */
 
 int main(void)
 {
