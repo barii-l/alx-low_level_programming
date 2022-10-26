8184da19464b80730b6e743930698e574ca75780
Bariyat Likita <bariyatlikita@gmail.com>
2022-10-26 23:15:30 +0100

    corrections to task 6. cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,35 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
-* cap_string - a function that capitalizes all  words of a string.
-* @s: An input string to capitalize letters
-* Return: Pointer to s
-*/
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
 char *cap_string(char *s)
 {
-int i = 0;
-while (s[i])
-{
-if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-{
-s[i] -= 32;
-}
-if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-{
-s[i + 1] -= 32;
-}
-i++;
-}
-return (s);
-}
-/**
-* check_separators - separator of words: space, tabulation, new line,
-* ,, ;, ., !, ?, (, ), {, and }
-* @c: An input character
-* Return: 1 if separator,  0 otherwise
-*/
-int check_separators(char c)
-{
-int i = 0;
-char separators[13] = { ' ', '\t', '\n', ',', ';', '.', '!',
-'?', '"', '(', ')', '{', '}' };
-for (; i < 13; i++)
-{
-if (c == separators[i])
-{
-return (1);
-}
-}
-return (0);
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+		i++;
+	}
+	return (s);
 }
