1bb331c4dc834b902d5461ec11ac8a882104f93b
Bariyat Likita <bariyatlikita@gmail.com>
2022-10-26 23:17:33 +0100

    corrections to task 7. leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,33 @@
 #include "main.h"
-
 /**
-* leet - a function that encodes a string into 1337.
-* @s: An input string to encode
-* Return: An encoded string
-*/
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
+ */
 char *leet(char *s)
 {
-int i = 0, j;
-char temp[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-while (s[i])
-{
-for (j = 0;  j < 10; j++)
-{
-if (s[i] == str[j])
-{
-s[i] = subs[j];
-}
-}
-i++
-}
-return (s);
+	int i;
+	int j;
+	int k;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 }
